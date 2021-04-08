e90713122fddbb859dcf4946825b8a4ed90b19b2
Jossl123 <jossl1.gau@gmail.com291204Jg291204JG>
2021-04-08 16:12:27 +0200

    add deezer request

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 var webkitSpeechRecognition = window.webkitSpeechRecognition;
 var recognition = new webkitSpeechRecognition();
 
+fetch('https://connect.deezer.com/oauth/auth.php?app_id=471962&redirect_uri=https://jossl123.github.io/MyDeezerAssistant/&perms=basic_access,email')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
 // This runs when the speech recognition service starts
 recognition.onstart = function() {
     console.log("We are listening. Try speaking into the microphone.");
@@ -9,6 +16,7 @@ recognition.onstart = function() {
 // This runs when the speech recognition service returns result
 recognition.onresult = function(event) {
     var transcript = event.results[0][0].transcript;
+
 };
 
 // start recognition
