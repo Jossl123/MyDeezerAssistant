101fc9b7b4982c0d2387dfd6410123e34178d051
Jossl123 <jossl1.gau@gmail.com291204Jg291204JG>
2021-04-11 16:04:26 +0200

    begin the voice script

--- js/index.js
+++ js/index.js
@@ -1,9 +1,49 @@
-var webkitSpeechRecognition = window.webkitSpeechRecognition;
-var recognition = new webkitSpeechRecognition();
+const magic_word = "coucou";
+
+// initialize our SpeechRecognition object
+let recognition = new webkitSpeechRecognition();
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+recognition.continuous = true;
+
+// detect the magic word
+recognition.onresult = e => {
+    var result = e.results[0][0].transcript
+    console.log(result)
+    if (result.includes("deezer") || result.includes("10h") || result.includes("Deezer")) {
+        var param = result.replace('10h', '').replace('deezer', '').replace('Deezer', '')
+        param = param.split(" ")
+        if (param.indexOf("volume") >= -1) {
+            if (param.indexOf("plus") >= -1) {
+                console.log("augmenter le volume")
+            } else if (param.indexOf("moins") >= -1) {
+                console.log("baisser le volume")
+            }
+        }
+    } else {
+        console.log("pas deezer")
+    }
+}
+
+// called when we detect sound
+setInterval(function() {
+    try { // calling it twice will throw...
+        recognition.start();
+    } catch (e) {}
+}, 5000)
+
 
 DZ.init({
     appId: '471962',
-    channelUrl: 'http://jossl123.github.io/MyDeezerAssistant/channel.html'
+    channelUrl: 'http://jossl123.github.io/MyDeezerAssistant/channel.html',
+    player: {
+        container: 'player',
+        width: 300,
+        height: 300,
+        format: 'square',
+        onload: function() {}
+    }
+
 });
 
 function login() {
@@ -18,17 +58,3 @@ function login() {
         }
     }, { perms: 'basic_access,email' });
 }
-
-// This runs when the speech recognition service starts
-/*recognition.onstart = function() {
-    console.log("We are listening. Try speaking into the microphone.");
-};
-
-// This runs when the speech recognition service returns result
-recognition.onresult = function(event) {
-    var transcript = event.results[0][0].transcript;
-
-};
-
-// start recognition
-recognition.start();*/
